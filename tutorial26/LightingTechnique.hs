8b9c4df284a05f0dfe7199ca5cb189325bd565de
Ilya Smelkov <triplepointfive@gmail.com>
2015-11-08 07:15:02 +0300

    CP 26

---
+++ tutorial26/LightingTechnique.hs
@@ -0,0 +1,278 @@
+{-# LANGUAGE RecordWildCards #-}
+module Hogldev.LightingTechnique (
+    LightingTechnique(..)
+  , DirectionLight(..)
+  , PointLight(..)
+  , DirectionalLight(..)
+  , changeAmbIntensity
+  , changeDiffIntensity
+  , initLightingTechnique
+  , setLightingWVP
+  , setLightingWorldMatrix
+  , setLightingTextureUnit
+  , setDirectionalLight
+  , setEyeWorldPos
+  , setMatSpecularPower
+  , setMaterialSpecularIntensity
+  , setPointLights
+  , setSpotLights
+  , enableLightingTechnique
+) where
+
+import           Graphics.GLUtil
+import           Graphics.Rendering.OpenGL
+
+import           Hogldev.Technique
+import           Hogldev.Utils (normalizeVertex, toRadian)
+
+maxPointLights = 2
+maxSpotLights  = 2
+
+data DirectionLight =
+    DirectionLight
+    { ambientColor     :: !(Vertex3 GLfloat)
+    , ambientIntensity :: !GLfloat
+    , diffuseDirection :: !(Vertex3 GLfloat)
+    , diffuseIntensity :: !GLfloat
+    } deriving Show
+
+data PointLight =
+    PointLight
+    { pAmbientColor     :: !(Vertex3 GLfloat)
+    , pAmbientIntensity :: !GLfloat
+    , pDiffuseIntensity :: !GLfloat
+    , pPosition         :: !(Vertex3 GLfloat)
+    , pConstant         :: !GLfloat
+    , pLinear           :: !GLfloat
+    , pExp              :: !GLfloat
+    } deriving Show
+
+data DirectionalLight =
+    DirectionalLight
+    { dAmbientColor     :: !(Vertex3 GLfloat)
+    , dAmbientIntensity :: !GLfloat
+    , dDiffuseIntensity :: !GLfloat
+    , dPosition         :: !(Vertex3 GLfloat)
+    , dConstant         :: !GLfloat
+    , dLinear           :: !GLfloat
+    , dExp              :: !GLfloat
+    , dDirection        :: !(Vertex3 GLfloat)
+    , dCutOff           :: !GLfloat
+    }
+
+changeAmbIntensity :: (GLfloat -> GLfloat) -> DirectionLight -> DirectionLight
+changeAmbIntensity f dir@DirectionLight{..} =
+    dir { ambientIntensity = f ambientIntensity }
+
+changeDiffIntensity :: (GLfloat -> GLfloat) -> DirectionLight -> DirectionLight
+changeDiffIntensity f dir@DirectionLight{..} =
+    dir { diffuseIntensity = f diffuseIntensity }
+
+data LightingTechnique =
+    LightingTechnique
+    { lProgram                          :: !Program
+    , lWVPLoc                           :: !UniformLocation
+    , lWorldMatrixLoc                   :: !UniformLocation
+    , lSamplerLoc                       :: !UniformLocation
+    , lDirLightColorLoc                 :: !UniformLocation
+    , lDirLightAmbientIntensityColorLoc :: !UniformLocation
+    , lDirLightDirectionLoc             :: !UniformLocation
+    , lDirLightIntensity                :: !UniformLocation
+    , lEyeWorldPosLoc                   :: !UniformLocation
+    , lMatSpecularIntensityLoc          :: !UniformLocation
+    , lMatSpecularPowerLoc              :: !UniformLocation
+    , lNumPointLightsLoc                :: !UniformLocation
+    , lPointLightLocs                   :: ![PointLightLoc]
+    , lNumSpotLightsLoc                 :: !UniformLocation
+    , lSpotLightLocs                    :: ![DirectionalLightLoc]
+    }
+
+data PointLightLoc =
+    PointLightLoc
+    { plColorLoc            :: !UniformLocation
+    , plAmbientIntensityLoc :: !UniformLocation
+    , plDiffuseIntensityLoc :: !UniformLocation
+    , plPositionLoc         :: !UniformLocation
+    , plConstantLoc         :: !UniformLocation
+    , plLinearLoc           :: !UniformLocation
+    , plExpLoc              :: !UniformLocation
+    }
+
+data DirectionalLightLoc =
+    DirectionalLightLoc
+    { dlColorLoc            :: !UniformLocation
+    , dlAmbientIntensityLoc :: !UniformLocation
+    , dlDiffuseIntensityLoc :: !UniformLocation
+    , dlPositionLoc         :: !UniformLocation
+    , dlConstantLoc         :: !UniformLocation
+    , dlLinearLoc           :: !UniformLocation
+    , dlExpLoc              :: !UniformLocation
+    , dlDirectionLoc        :: !UniformLocation
+    , dlCutOffLoc           :: !UniformLocation
+    }
+
+initLightingTechnique :: IO LightingTechnique
+initLightingTechnique = do
+    program <- createProgram
+    addShader program "tutorial26/lighting.vs" VertexShader
+    addShader program "tutorial26/lighting.fs" FragmentShader
+    finalize program
+
+    wvpLoc <- getUniformLocation program "gWVP"
+    worldMatrixLoc <- getUniformLocation program "gWorld"
+    samplerLoc <- getUniformLocation program "gSampler"
+    eyeWorldPosition <- getUniformLocation program "gEyeWorldPos"
+
+    dirLightColorLoc <- getUniformLocation program "gDirectionalLight.Base.Color"
+    dirLightAmbientIntensityLoc <- getUniformLocation program
+        "gDirectionalLight.Base.AmbientIntensity"
+    dirLightDirectionLoc <- getUniformLocation program
+        "gDirectionalLight.Direction"
+    dirLightDiffuseIntensity <- getUniformLocation program
+        "gDirectionalLight.Base.DiffuseIntensity"
+
+    matSpecularIntensity <- getUniformLocation program "gMatSpecularIntensity"
+    matSpecularPower <- getUniformLocation program "gSpecularPower"
+
+    numPointLightsLoc <- getUniformLocation program "gNumPointLights"
+    pointLights <- mapM (pointLightLoc program) [0..maxPointLights-1]
+
+    numSpotLights <- getUniformLocation program "gNumSpotLights"
+    spotLights <- mapM (spotLightLoc program) [0..maxSpotLights-1]
+
+    return LightingTechnique
+        { lProgram                          = program
+        , lWVPLoc                           = wvpLoc
+        , lWorldMatrixLoc                   = worldMatrixLoc
+        , lSamplerLoc                       = samplerLoc
+        , lDirLightColorLoc                 = dirLightColorLoc
+        , lDirLightAmbientIntensityColorLoc = dirLightAmbientIntensityLoc
+        , lDirLightDirectionLoc             = dirLightDirectionLoc
+        , lDirLightIntensity                = dirLightDiffuseIntensity
+        , lEyeWorldPosLoc                   = matSpecularIntensity
+        , lMatSpecularIntensityLoc          = eyeWorldPosition
+        , lMatSpecularPowerLoc              = matSpecularPower
+        , lNumPointLightsLoc                = numPointLightsLoc
+        , lPointLightLocs                   = pointLights
+        , lNumSpotLightsLoc                 = numSpotLights
+        , lSpotLightLocs                    = spotLights
+        }
+
+pointLightLoc :: Program -> Int -> IO PointLightLoc
+pointLightLoc program index = do
+    lColor            <- loc "Base.Color"
+    lAmbientIntensity <- loc "Base.AmbientIntensity"
+    lPosition         <- loc "Position"
+    lDiffuseIntensity <- loc "Base.DiffuseIntensity"
+    lConstant         <- loc "Atten.Constant"
+    lLinear           <- loc "Atten.Linear"
+    lExp              <- loc "Atten.Exp"
+    return PointLightLoc
+        { plColorLoc            = lColor
+        , plAmbientIntensityLoc = lAmbientIntensity
+        , plDiffuseIntensityLoc = lDiffuseIntensity
+        , plPositionLoc         = lPosition
+        , plConstantLoc         = lConstant
+        , plLinearLoc           = lLinear
+        , plExpLoc              = lExp
+        }
+  where
+    loc field = getUniformLocation program
+        ("gPointLights[" ++ show index ++ "]." ++ field)
+
+spotLightLoc :: Program -> Int -> IO DirectionalLightLoc
+spotLightLoc program index = do
+    dlColorLoc            <- loc "Base.Base.Color"
+    dlAmbientIntensityLoc <- loc "Base.Base.AmbientIntensity"
+    dlDiffuseIntensityLoc <- loc "Base.Base.DiffuseIntensity"
+    dlPositionLoc         <- loc "Base.Position"
+    dlConstantLoc         <- loc "Base.Atten.Constant"
+    dlLinearLoc           <- loc "Base.Atten.Linear"
+    dlExpLoc              <- loc "Base.Atten.Exp"
+    dlDirectionLoc        <- loc "Direction"
+    dlCutOffLoc           <- loc "Cutoff"
+    return DirectionalLightLoc
+        { dlColorLoc            = dlColorLoc
+        , dlAmbientIntensityLoc = dlAmbientIntensityLoc
+        , dlDiffuseIntensityLoc = dlDiffuseIntensityLoc
+        , dlPositionLoc         = dlPositionLoc
+        , dlConstantLoc         = dlConstantLoc
+        , dlLinearLoc           = dlLinearLoc
+        , dlExpLoc              = dlExpLoc
+        , dlDirectionLoc        = dlDirectionLoc
+        , dlCutOffLoc           = dlCutOffLoc
+        }
+  where
+    loc field = getUniformLocation program
+        ("gSpotLights[" ++ show index ++ "]." ++ field)
+
+setLightingWVP :: LightingTechnique -> [[GLfloat]] -> IO ()
+setLightingWVP LightingTechnique{..} mat = uniformMat lWVPLoc $= mat
+
+setLightingWorldMatrix :: LightingTechnique -> [[GLfloat]] -> IO ()
+setLightingWorldMatrix LightingTechnique{..} mat =
+    uniformMat lWorldMatrixLoc $= mat
+
+setLightingTextureUnit :: LightingTechnique -> GLuint -> IO ()
+setLightingTextureUnit LightingTechnique{..} textureUnit =
+    uniformScalar lSamplerLoc $= textureUnit
+
+setDirectionalLight :: LightingTechnique -> DirectionLight -> IO ()
+setDirectionalLight LightingTechnique{..} DirectionLight{..} = do
+    uniformVec lDirLightColorLoc $= [cx, cy, cz]
+    uniformScalar lDirLightAmbientIntensityColorLoc $= ambientIntensity
+    uniformVec lDirLightDirectionLoc $= [lx, ly, lz]
+    uniformScalar lDirLightIntensity $= diffuseIntensity
+  where
+    (Vertex3 cx cy cz) = ambientColor
+    (Vertex3 lx ly lz) = diffuseDirection
+
+setMaterialSpecularIntensity :: LightingTechnique -> GLfloat -> IO ()
+setMaterialSpecularIntensity LightingTechnique{..} intensity =
+    uniformScalar lSamplerLoc $= intensity
+
+setMatSpecularPower :: LightingTechnique -> GLfloat -> IO ()
+setMatSpecularPower LightingTechnique{..} power =
+    uniformScalar lMatSpecularPowerLoc $= power
+
+setEyeWorldPos :: LightingTechnique -> Vector3 GLfloat -> IO ()
+setEyeWorldPos LightingTechnique{..} (Vector3 x y z) =
+    uniformVec lEyeWorldPosLoc $= [x, y, z]
+
+setPointLights :: LightingTechnique -> GLint -> [PointLight] -> IO ()
+setPointLights LightingTechnique{..} numLights pointLights = do
+    uniformScalar lNumPointLightsLoc $= numLights
+    mapM_ (uncurry setPointLight) (zip pointLights lPointLightLocs)
+  where
+    setPointLight :: PointLight -> PointLightLoc -> IO ()
+    setPointLight PointLight{..} PointLightLoc{..} = do
+        uniformVertex3 plColorLoc pAmbientColor
+        uniformVertex3 plPositionLoc pPosition
+        uniformScalar plAmbientIntensityLoc $= pAmbientIntensity
+        uniformScalar plDiffuseIntensityLoc $= pDiffuseIntensity
+        uniformScalar plConstantLoc         $= pConstant
+        uniformScalar plLinearLoc           $= pLinear
+        uniformScalar plExpLoc              $= pExp
+
+uniformVertex3 :: UniformLocation -> Vertex3 GLfloat -> IO ()
+uniformVertex3 loc (Vertex3 x y z) = uniformVec loc $= [x, y, z]
+
+setSpotLights :: LightingTechnique -> GLint -> [DirectionalLight] ->  IO ()
+setSpotLights LightingTechnique{..} numLights spotLights = do
+    uniformScalar lNumSpotLightsLoc $= numLights
+    mapM_ (uncurry setSpotLight) (zip spotLights lSpotLightLocs)
+  where
+    setSpotLight :: DirectionalLight -> DirectionalLightLoc -> IO ()
+    setSpotLight DirectionalLight{..}  DirectionalLightLoc{..} = do
+        uniformVertex3 dlColorLoc dAmbientColor
+        uniformVertex3 dlPositionLoc dPosition
+        uniformVertex3 dlDirectionLoc (normalizeVertex dDirection)
+        uniformScalar dlAmbientIntensityLoc $= dAmbientIntensity
+        uniformScalar dlDiffuseIntensityLoc $= dDiffuseIntensity
+        uniformScalar dlConstantLoc         $= dConstant
+        uniformScalar dlLinearLoc           $= dLinear
+        uniformScalar dlExpLoc              $= dExp
+        uniformScalar dlCutOffLoc           $= cos (toRadian dCutOff)
+
+enableLightingTechnique :: LightingTechnique -> IO ()
+enableLightingTechnique LightingTechnique{..} = enableTechnique lProgram
---
+++ tutorial26/Tutorial26.hs
@@ -0,0 +1,149 @@
+module Main where
+
+import           Data.IORef
+import           Graphics.Rendering.OpenGL
+import           Graphics.UI.GLUT hiding (exit)
+import           System.Exit (exitSuccess)
+
+import           Hogldev.Pipeline (
+                    Pipeline(..), getTrans,
+                    PersProj(..)
+                 )
+import           Hogldev.Camera ( Camera(..), cameraOnKeyboard,
+                    initCamera, cameraOnMouse, cameraOnRender
+                 )
+import           Hogldev.CubemapTexture (CubeMapFilenames(..))
+
+import           Skybox
+import           Hogldev.LightingTechnique
+import           Mesh
+
+windowWidth = 1920
+windowHeight = 1080
+
+persProjection = PersProj
+                 { persFOV   = 60
+                 , persWidth = fromIntegral windowWidth
+                 , persHeigh = fromIntegral windowHeight
+                 , persZNear = 1
+                 , persZFar  = 100
+                 }
+
+dirLight :: DirectionLight
+dirLight = DirectionLight
+    { ambientColor     = Vertex3 1 1 1
+    , ambientIntensity = 0.2
+    , diffuseIntensity = 0.8
+    , diffuseDirection = Vertex3 1.0 (-1.0) 0.0
+    }
+
+main :: IO ()
+main = do
+    getArgsAndInitialize
+    initialDisplayMode $= [DoubleBuffered, RGBAMode, WithDepthBuffer]
+    initialWindowSize $= Size windowWidth windowHeight
+    initialWindowPosition $= Position 100 100
+    createWindow "Tutorial 26"
+
+    -- frontFace $= CW
+    -- cullFace $= Just Back
+    depthFunc $= Just Lequal
+
+    gScale <- newIORef 0.0
+    cameraRef <- newIORef newCamera
+
+    lightingEffect <- initLightingTechnique
+    enableLightingTechnique lightingEffect
+    -- Set color texture unit.
+    setDirectionalLight lightingEffect dirLight
+    setLightingTextureUnit lightingEffect 0
+
+    pointerPosition $= mousePos
+
+    tankMesh <- loadMesh "assets/phoenix_ugv.md2"
+    skyBox <- initSkybox persProjection cubeMapFilenames
+
+    initializeGlutCallbacks tankMesh lightingEffect gScale cameraRef skyBox
+    clearColor $= Color4 0 0 0 0
+
+    mainLoop
+  where
+    newCamera = initCamera (Just
+        (Vector3 0 1 (-20), Vector3 0 0 1, Vector3 0 1 0)
+        ) windowWidth windowHeight
+    mousePos = Position (windowWidth `div` 2) (windowHeight `div` 2)
+
+    cubeMapFilenames = CubeMapFilenames
+        { directory    = "assets"
+        , posXFilename = "sp3right.jpg"
+        , negXFilename = "sp3left.jpg"
+        , posYFilename = "sp3top.jpg"
+        , negYFilename = "sp3bot.jpg"
+        , posZFilename = "sp3front.jpg"
+        , negZFilename = "sp3back.jpg"
+        }
+
+initializeGlutCallbacks :: Mesh
+                        -> LightingTechnique
+                        -> IORef GLfloat
+                        -> IORef Camera
+                        -> Skybox
+                        -> IO ()
+initializeGlutCallbacks tankMesh lightingEffect gScale cameraRef texture = do
+    displayCallback $=
+        renderSceneCB tankMesh lightingEffect gScale cameraRef texture
+    idleCallback    $= Just (idleCB gScale cameraRef)
+    specialCallback $= Just (specialKeyboardCB cameraRef)
+    keyboardCallback $= Just keyboardCB
+    passiveMotionCallback $= Just (passiveMotionCB cameraRef)
+
+keyboardCB :: KeyboardCallback
+keyboardCB 'q' _ = exitSuccess
+keyboardCB _ _ = return ()
+
+specialKeyboardCB :: IORef Camera -> SpecialCallback
+specialKeyboardCB cameraRef key _ = cameraRef $~! cameraOnKeyboard key
+
+passiveMotionCB :: IORef Camera -> MotionCallback
+passiveMotionCB cameraRef position = cameraRef $~! cameraOnMouse position
+
+idleCB :: IORef GLfloat -> IORef Camera -> IdleCallback
+idleCB gScale cameraRef = do
+    gScale $~! (+ 0.01)
+    cameraRef $~! cameraOnRender
+    postRedisplay Nothing
+
+renderSceneCB :: Mesh
+              -> LightingTechnique
+              -> IORef GLfloat
+              -> IORef Camera
+              -> Skybox
+              -> DisplayCallback
+renderSceneCB tankMesh lightingEffect gScale cameraRef skyBox = do
+    cameraRef $~! cameraOnRender
+    gScaleVal <- readIORef gScale
+    camera <- readIORef cameraRef
+
+    clear [ColorBuffer, DepthBuffer]
+
+    enableLightingTechnique lightingEffect
+
+    setLightingWVP lightingEffect $ getTrans
+        WVPPipeline {
+            worldInfo  = Vector3 0 (-5) 3,
+            scaleInfo  = Vector3 0.1 0.1 0.1,
+            rotateInfo = Vector3 (-90) gScaleVal 0,
+            persProj   = persProjection,
+            pipeCamera = camera
+        }
+    setLightingWorldMatrix lightingEffect $ getTrans
+        WPipeline {
+            worldInfo  = Vector3 0 (-5) 3,
+            scaleInfo  = Vector3 0.1 0.1 0.1,
+            rotateInfo = Vector3 (-90) gScaleVal 0
+        }
+
+    renderMesh tankMesh
+    skyboxRender skyBox camera
+
+    swapBuffers
