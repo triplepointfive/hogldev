466ce6873cf6ec177d678a6be677d97b1b2bacfd
Ilya Smelkov <triplepointfive@gmail.com>
2015-11-25 07:57:28 +0300

    Render & update particles

--- tutorial28/ParticleSystem.hs
+++ tutorial28/ParticleSystem.hs
@@ -10,7 +10,8 @@ import           Foreign.Ptr
 
 import           Graphics.Rendering.OpenGL
 
-import           Hogldev.RandomTexture
+import           Hogldev.RandomTexture as RT
+import           Hogldev.Texture as T
 import           Hogldev.Math3D (Matrix4)
 import           BillboardTechnique
 import           PSUpdateTechnique
@@ -41,10 +42,10 @@ instance Storable Particle where
 data ParticleSystem =
     ParticleSystem
     { isFirst           :: !Bool
-    , currVB            :: !BufferObject
-    , currTFB           :: !BufferObject
-    , particleBuffer    :: ![GLuint]
-    , transformFeedback :: ![GLuint]
+    , currVB            :: !Int
+    , currTFB           :: !Int
+    , particleBuffer    :: ![BufferObject]
+    , transformFeedback :: ![BufferObject]
     , psUpdateTech      :: !PSUpdateTechnique
     , billboardTech     :: !BillboardTechnique
     , randTexture       :: !RandomTexture
@@ -65,9 +66,91 @@ renderPS deltaTimeMillis vp cameraPos particleSystem = do
   where
     ps = particalSystem { time = time particleSystem }
 
+    vPosition = AttribLocation 0
+    vTextCoord = AttribLocation 1
+    vNormals = AttribLocation 2
+    vTangent = AttribLocation 3
+
     updateParticles :: IO ()
     updateParticles = undefined
+        enablePSUpdateTechnique
+        setTime
+        setDeltaTimeMillis
+        RT.textureBind (randTexture ps) RandomTexture(4)
+
+        rasterizerDiscard $= Enabled
+
+        bindBuffer ArrayBuffer $= Just (particleBuffer ps !! currVB ps)
+        -- bindTransformFeedback
+
+        vertexAttribArray vPosition $= Enabled
+        vertexAttribArray vTextCoord $= Enabled
+        vertexAttribArray vNormals $= Enabled
+        vertexAttribArray vTangent $= Enabled
+
+        vertexAttribPointer vPosition $=
+            ( ToFloat
+            , VertexArrayDescriptor 3 Float (fromIntegral vertexSize)
+              (bufferOffset 0)
+            )
+        vertexAttribPointer vTextCoord $=
+            ( ToFloat
+            , VertexArrayDescriptor 2 Float (fromIntegral vertexSize)
+              (bufferOffset (sizeOf (Vertex3 0 0 0 :: Vertex3 GLfloat)))
+            )
+        vertexAttribPointer vNormals $=
+            ( ToFloat
+            , VertexArrayDescriptor 3 Float (fromIntegral vertexSize)
+              (bufferOffset (
+                  sizeOf (Vertex3 0 0 0 :: Vertex3 GLfloat)
+                + sizeOf (TexCoord2 0 0 :: TexCoord2 GLfloat))
+              )
+            )
+        vertexAttribPointer vTangent $=
+            ( ToFloat
+            , VertexArrayDescriptor 3 Float (fromIntegral vertexSize)
+              (bufferOffset (
+                  sizeOf (Vertex3 0 0 0 :: Vertex3 GLfloat)    -- Position
+                + sizeOf (TexCoord2 0 0 :: TexCoord2 GLfloat)  -- Texture
+                + sizeOf (Vertex3 0 0 0 :: Vertex3 GLfloat))   -- Normal
+              )
+            )
+
+        beginTransformFeedback Points
+
+        if (isFirst ps)
+          then drawA
+            drawArrays Points 0 1
+            -- isFirst = false
+          else
+            -- drawTransformFeedback
+
+        endTransfromFeedback
+
+        vertexAttribArray vPosition $= Disabled
+        vertexAttribArray vTextCoord $= Disabled
+        vertexAttribArray vNormals $= Disabled
+        vertexAttribArray vTangent $= Disabled
 
     renderParticles :: IO ()
-    renderParticles = undefined
+    renderParticles = do
+        enableBillboardTechnique billboardTech
+        setBillboardCameraPosition billboardTech cameraPos
+        setBillboardTechniqueVP billboardTech vp
+        T.textureBind (texture ps) (TextureUnit 0)
 
+        rasterizerDiscard $= Disabled
+
+        bindBuffer ArrayBuffer $= Just (particleBuffer ps !! currVB ps)
+
+        vertexAttribArray vPosition $= Enabled
+
+        vertexAttribPointer vPosition $=
+            ( ToFloat
+            , VertexArrayDescriptor 3 Float (fromIntegral vertexSize)
+              (bufferOffset (sizeOf (0 :: GLfloat)))
+            )
+
+        -- drawTransformFeedback
+
+        vertexAttribArray vPosition $= Disabled
